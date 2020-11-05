71b31a2e32b8a2e23935c9a4be841912c455c774
igorigor202 <igorigorcs2@gmail.com>
2020-11-06 00:56:06 +0300

    2 task index.js

---
+++ 1-module/2-task/index.js
@@ -0,0 +1,21 @@
+function print(text) {
+  console.log(text);
+}
+
+
+function isValid(name) {
+  if (!name || name.indexOf(' ') !== -1 || name.length < 4) {
+    return false;
+  }
+  return true;
+}
+
+function sayHello() {
+  let userName = prompt('Введите ваше имя');
+
+  if (isValid(userName)) {
+    print(`Welcome back, ${userName}!`);
+  } else {
+    print('Некорректное имя');
+  }
+}
